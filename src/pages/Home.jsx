e97ba185aeb6a5b1985b6c1001dbd7ac12dd34f6
Suegathh <suzzainnegathu@gmail.com>
2025-03-05 10:23:11 +0300

    styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
   return (
     <div id="home">
       <div className="img-wra">
-        <img src=".\images\hotel1.jpeg" alt="" />
+        <img src="./images/hotel1.jpeg" alt="Hotel" />
       </div>
     </div>
   );
