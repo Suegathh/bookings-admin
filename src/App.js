4bea1c362992f64864fda9a4e7011920193c15b5
Suegathh <suzzainnegathu@gmail.com>
2025-03-07 00:18:36 +0300

    add responsitivity

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
+
 import Header from "./components/Header";
 import Dashboard from "./pages/Dashboard";
 import CreateRoom from "./pages/CreateRoom";
@@ -20,7 +20,6 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/roomList" element={<RoomList />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/rooms" element={<Rooms />} />
           <Route path="/rooms/create" element={<CreateRoom />} />
