837c9bb583a42874c4e6d69aba540a5eac536f64
Suegathh <suzzainnegathu@gmail.com>
2025-03-06 21:26:34 +0300

    remove register

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
 import Header from "./components/Header";
 import Dashboard from "./pages/Dashboard";
 import CreateRoom from "./pages/CreateRoom";
@@ -22,7 +21,6 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/roomList" element={<RoomList />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/rooms" element={<Rooms />} />
           <Route path="/rooms/create" element={<CreateRoom />} />
